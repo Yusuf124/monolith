d3e115bbd580998394b7cc1d2f46c39d8506386e
ypapy <ypapy22@gmail.com>
2022-07-09 18:25:25 +0200

    project pushing

---
+++ udagram-api-user/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=ypapy
+export POSTGRES_PASSWORD=Taufiq01*
+export POSTGRES_HOST=cdr.cqjiydrctj7z.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::refactor-monolith-udacity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=esryiiiiiiiiiidstyiudes
+export URL=http://localhost:8100
