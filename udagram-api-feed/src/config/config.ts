716748fc79a6e530e882fa3e17d81e571cd7f7fd
ypapy <ypapy22@gmail.com>
2022-08-07 22:36:38 +0200

    solving a connection problem

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,14 +1,15 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'ypapy',
+  'password': 'Taufiq01*',
+  'database': 'postgres',
+  'host': 'monolithmicroservice.crn3d9pz41du.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
+  'aws_region': 'us-east-1',
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'url': ' http://localhost:8100 ',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'whateverrrr',
   },
 };
+
